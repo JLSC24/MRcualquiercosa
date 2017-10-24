a80714f7224c94b5e60e277fc3927196f1c48743
John Arroyave <unix4you2@gmail.com>
2017-10-24 07:24:21 -0500

    Update miarchivo.php

--- miarchivo.php
+++ miarchivo.php
@@ -3,6 +3,20 @@ $NombePersona="Pepito";
 //error
 echo "Hola ".$NombePersona
 
+  function ProbarBD()
+{
+if ($OK)
+  return 0;
+if(!$OK)
+  return 1;
+}
+  
+  
+  ProbarBD();
+  
+return 1;
+  
+  
 ?>
 
 
